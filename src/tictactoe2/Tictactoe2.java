e743683cfaf75ff4f298c582c84446f524fe7ae8
GioRosario <74158598+GioRosario@users.noreply.github.com>
2022-07-02 15:07:34 +0800

    Update Tictactoe2.java

--- src/tictactoe2/Tictactoe2.java
+++ src/tictactoe2/Tictactoe2.java
@@ -22,7 +22,8 @@ public class Tictactoe2 extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
                 
         Scene scene = new Scene(root);
-                
+        
+        stage.setTitle("Tic Tac Toe");
         stage.setScene(scene);
         stage.show();
     }
